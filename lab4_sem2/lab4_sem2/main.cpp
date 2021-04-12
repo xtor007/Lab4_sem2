a300962693a69bf3d73323adbad417ce402f12f2
Anatoliy <tolxpams@gmail.com>
2021-04-12 09:36:45 +0300

    added sample change

--- lab4_sem2/lab4_sem2/main.cpp
+++ lab4_sem2/lab4_sem2/main.cpp
@@ -6,9 +6,19 @@
 //
 
 #include <iostream>
+#include <vector>
+#include "sample.hpp"
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-   
+    vector<int8_t> k;
+    k.push_back(2);
+    k.push_back(3);
+    k.push_back(4);
+    k.push_back(4);
+    Samples kk = Samples(k);
+    kk.changeSampl(2);
+    vector<int8_t> kkk = kk.getSample();
+    
     return 0;
 }
--- lab4_sem2/lab4_sem2/sample.cpp
+++ lab4_sem2/lab4_sem2/sample.cpp
@@ -11,6 +11,18 @@ vector<int8_t> Samples::getSample() {
     return sampleSet;
 }
 
-void Samples::changeSampl(int n) {
-    
+void Samples::changeSampl(float n) {
+    n = 1/n;
+    float posSimpl = 0;
+    vector<int8_t> newSampleSet;
+    int endOfSegment = sampleSet.size()-1;
+    while (posSimpl < endOfSegment) {
+        if (posSimpl - int(posSimpl) == 0) {
+            newSampleSet.push_back(sampleSet[int(posSimpl)]);
+        } else {
+            newSampleSet.push_back(sampleSet[int(posSimpl)] + (posSimpl-int(posSimpl))*(sampleSet[int(posSimpl)+1]+sampleSet[int(posSimpl)]));
+        }
+        posSimpl += n;
+    }
+    sampleSet = newSampleSet;
 }
