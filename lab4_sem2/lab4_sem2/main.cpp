4eeb627b816c7c77bee43a92c10ae3657a5ed35b
Anatoliy <tolxpams@gmail.com>
2021-04-16 17:53:19 +0300

    removed all unnecessary

--- lab4_sem2/lab4_sem2/main.cpp
+++ lab4_sem2/lab4_sem2/main.cpp
@@ -11,11 +11,5 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    int8_t k[3] = {'\x02','\x04','\x03'};
-    Samples kk = Samples(k, 3);
-    kk.changeSampl(2);
-    int8_t kkk[5];
-    kk.getSample(kkk);
-    
     return 0;
 }
