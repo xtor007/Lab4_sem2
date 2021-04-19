111664a22aba17a00b5318dbb65dce1c64192e0b
berlinsky <berlinsky.yar@gmail.com>
2021-04-19 18:03:39 +0300

    creating sound.cpp/hpp and basic structures

---
+++ lab4_sem2/lab4_sem2/sound.cpp
@@ -0,0 +1,8 @@
+//
+//  sound.cpp
+//  lab4_sem2
+//
+//  Created by Берлинский Ярослав Владленович on 19.04.2021.
+//
+
+#include "sound.hpp"
---
+++ lab4_sem2/lab4_sem2/sound.hpp
@@ -0,0 +1,43 @@
+//
+//  sound.hpp
+//  lab4_sem2
+//
+//  Created by Берлинский Ярослав Владленович on 19.04.2021.
+//
+
+#ifndef sound_hpp
+#define sound_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+typedef struct{
+    int32_t chunkId;
+    int32_t chunkSize;
+                       
+                       
+    int32_t format;
+} RIFFHEADER;
+
+typedef struct{
+    int32_t subchunk1Id;
+    int32_t subchunk1Size;
+    int16_t audioFormat;
+    int16_t numChannels;
+    int32_t sampleRate;
+    int32_t byteRate;
+    int16_t blockAlign;
+    int16_t bitsPerSample;
+} SUBCHUNK1;
+
+typedef struct{
+    int32_t subchunk2Id;
+    int32_t subchunk2Size;
+    //int8_t[] data;
+} SUBCHUNK2;
+
+#endif /* sound_hpp */
