7c69cedd1aeb85e98bd10e89837c07b51b16e124
Anatoliy <tolxpams@gmail.com>
2021-04-19 22:36:12 +0300

    made it possible to work for 16 bit

--- lab4_sem2/lab4_sem2/sample.cpp
+++ lab4_sem2/lab4_sem2/sample.cpp
@@ -7,30 +7,61 @@
 
 #include "sample.hpp"
 
-void Samples::getSample(int8_t *fileSamlesSet) {
-    for (int i=0; i<sampleSet.size(); i++) {
-        fileSamlesSet[i] = sampleSet[i];
+template <typename T>
+void Samples<T>::getSample(T fileSamlesSet) {
+    if (typeOfSampl == 8) {
+        for (int i=0; i<sampleSet8.size(); i++) {
+            fileSamlesSet[i] = sampleSet8[i];
+        }
+    } else {
+        for (int i=0; i<sampleSet16.size(); i++) {
+            fileSamlesSet[i] = sampleSet16[i];
+        }
     }
 }
 
-Samples::Samples (int8_t *newSampleSet, int numberOfSampls) {
-    for (int i=0; i<numberOfSampls; i++) {
-        sampleSet.push_back(newSampleSet[i]);
+template <typename T>
+Samples<T>::Samples (T newSampleSet, int numberOfSampls, int16_t bitsPerSample) {
+    typeOfSampl = bitsPerSample;
+    if (typeOfSampl == 8) {
+        for (int i=0; i<numberOfSampls; i++) {
+            sampleSet8.push_back(newSampleSet[i]);
+        }
+    } else {
+        for (int i=0; i<numberOfSampls; i++) {
+            sampleSet16.push_back(newSampleSet[i]);
+        }
     }
 }
 
-void Samples::changeSampl(float n) {
+
+template <typename T>
+void Samples<T>::changeSampl(float n) {
     n = 1/n;
     float posSimpl = 0;
-    vector<int8_t> newSampleSet;
-    int endOfSegment = int(sampleSet.size()-1);
-    while (posSimpl <= endOfSegment) {
-        if (posSimpl - int(posSimpl) == 0) {
-            newSampleSet.push_back(sampleSet[int(posSimpl)]);
-        } else {
-            newSampleSet.push_back(sampleSet[int(posSimpl)] + (posSimpl-int(posSimpl))*(sampleSet[int(posSimpl)+1]-sampleSet[int(posSimpl)]));
+    vector<int8_t> newSampleSet8;
+    vector<int16_t> newSampleSet16;
+    if (typeOfSampl == 8) {
+        int endOfSegment = int(sampleSet8.size()-1);
+        while (posSimpl <= endOfSegment) {
+            if (posSimpl - int(posSimpl) == 0) {
+                newSampleSet8.push_back(sampleSet8[int(posSimpl)]);
+            } else {
+                newSampleSet8.push_back(sampleSet8[int(posSimpl)] + (posSimpl-int(posSimpl))*(sampleSet8[int(posSimpl)+1]-sampleSet8[int(posSimpl)]));
+            }
+            posSimpl += n;
         }
-        posSimpl += n;
+        sampleSet8 = newSampleSet8;
+    } else {
+        int endOfSegment = int(sampleSet16.size()-1);
+        while (posSimpl <= endOfSegment) {
+            if (posSimpl - int(posSimpl) == 0) {
+                newSampleSet16.push_back(sampleSet16[int(posSimpl)]);
+            } else {
+                newSampleSet16.push_back(sampleSet16[int(posSimpl)] + (posSimpl-int(posSimpl))*(sampleSet16[int(posSimpl)+1]-sampleSet16[int(posSimpl)]));
+            }
+            posSimpl += n;
+        }
+        sampleSet16 = newSampleSet16;
     }
-    sampleSet = newSampleSet;
 }
