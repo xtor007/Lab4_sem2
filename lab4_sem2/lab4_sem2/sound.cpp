751359960b442eec4ec24ad1d4e41421c9d6c06e
berlinsky <berlinsky.yar@gmail.com>
2021-04-19 18:41:27 +0300

    reading riff in .wav

--- lab4_sem2/lab4_sem2/sound.cpp
+++ lab4_sem2/lab4_sem2/sound.cpp
@@ -6,3 +6,17 @@
 //
 
 #include "sound.hpp"
+
+RIFFHEADER waveReader::getHeader(){
+    RIFFHEADER riffHead;
+    fread(&riffHead, sizeof(riffHead), 1, audiofile);
+    if (riffHead.chunkId != 1179011410) {   // decimal FOURCC-code of "RIFF"-note in .wav
+        cout<<"The file format doesn't match.\n";
+        exit(9);
+    }
+    if (riffHead.format != 1163280727){     // decimal WAVE-identifier in .wav
+        cout<<"The file audioformat doesn't match.\n";
+        exit(9);
+    };
+    return riffHead;
+}
